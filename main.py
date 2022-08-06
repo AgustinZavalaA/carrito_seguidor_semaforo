fb1d1547073c73fc7ec85a63772c25ab103cf606
AgustinZavalaA <56318577+AgustinZavalaA@users.noreply.github.com>
2022-08-06 11:38:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def main() -> None:
     GPIO.setup(m1_pins[0], GPIO.OUT)
     GPIO.setup(m1_pins[1], GPIO.OUT)
 
-    while True:
+    for i in range(3):
         GPIO.output(m1_pins[0], GPIO.HIGH)
         GPIO.output(m1_pins[1], GPIO.LOW)
         time.sleep(1)
@@ -20,3 +20,11 @@ def main() -> None:
         GPIO.output(m1_pins[0], GPIO.LOW)
         GPIO.output(m1_pins[1], GPIO.LOW)
         time.sleep(1)
+
+
+if __name__ == "__main__":
+    main()
+    GPIO.cleanup()
+    print("Done")
+    exit(0)
+    pass
