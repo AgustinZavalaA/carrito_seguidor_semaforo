17e4b5ae91c66bed0f150902c41b1dac71d055b4
AgustinZavalaA <56318577+AgustinZavalaA@users.noreply.github.com>
2022-08-06 14:59:58 -0500

    Update detector_linea.py

--- detector_linea.py
+++ detector_linea.py
@@ -22,80 +22,89 @@ def main() -> None:
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, ancho)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, alto)
 
-    while True:
-        # leer un frame del video
-        _, frame = cap.read()
-        # convertir el frame a escala de grises
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        # detectar lineas negras en el frame
-        lineas = cv2.threshold(gray, 50, 255, cv2.THRESH_BINARY_INV)[1]
-        # dilatar las lineas para quedarnos con las mas largas
-        lineas = cv2.dilate(lineas, None, iterations=2)
+    try:
 
-        lineas_cnts, lineas_hier = cv2.findContours(
-            lineas.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
-        )
+        while True:
+            # leer un frame del video
+            _, frame = cap.read()
+            # convertir el frame a escala de grises
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            # detectar lineas negras en el frame
+            lineas = cv2.threshold(gray, 50, 255, cv2.THRESH_BINARY_INV)[1]
+            # dilatar las lineas para quedarnos con las mas largas
+            lineas = cv2.dilate(lineas, None, iterations=2)
 
-        cv2.drawContours(frame, lineas_cnts, -1, (0, 255, 0), 3)
-
-        if len(lineas_cnts) >= 2:
-            areaArray = []
-            for i, c in enumerate(lineas_cnts):
-                area = cv2.contourArea(c)
-                areaArray.append(area)
-
-            # first sort the array by area
-            sorteddata = sorted(
-                zip(areaArray, lineas_cnts), key=lambda x: x[0], reverse=True
+            lineas_cnts, lineas_hier = cv2.findContours(
+                lineas.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
             )
 
-            linea_der = sorteddata[0][1]
-            linea_izq = sorteddata[1][1]
+            cv2.drawContours(frame, lineas_cnts, -1, (0, 255, 0), 3)
 
-            centros = []
-            for c in [linea_der, linea_izq]:
-                # compute the center of the contour
-                M = cv2.moments(c)
-                cX = int(M["m10"] / M["m00"])
-                cY = int(M["m01"] / M["m00"])
-                centros.append((cX, cY))
-                # draw the contour and center of the shape on the image
-                cv2.circle(frame, (cX, cY), 7, (0, 0, 255), -1)
+            if len(lineas_cnts) >= 2:
+                areaArray = []
+                for i, c in enumerate(lineas_cnts):
+                    area = cv2.contourArea(c)
+                    areaArray.append(area)
 
-            centro_final = (
-                (centros[0][0] + centros[1][0]) / 2,
-                (centros[0][1] + centros[1][1]) / 2,
-            )
-            cv2.circle(
-                frame, (int(centro_final[0]), int(centro_final[1])), 7, (255, 0, 0), -1
-            )
+                # first sort the array by area
+                sorteddata = sorted(
+                    zip(areaArray, lineas_cnts), key=lambda x: x[0], reverse=True
+                )
 
-            print(centro_final[0], ancho / 2)
-            peso = 30
+                linea_der = sorteddata[0][1]
+                linea_izq = sorteddata[1][1]
 
-            if centro_final[0] < ancho / 2 - peso:
-                GPIO.output(m1_pins[0], GPIO.HIGH)
-                GPIO.output(m1_pins[1], GPIO.LOW)
-                GPIO.output(m2_pins[1], GPIO.HIGH)
-                GPIO.output(m2_pins[0], GPIO.LOW)
-            elif centro_final[0] > ancho / 2 + peso:
-                GPIO.output(m1_pins[0], GPIO.LOW)
-                GPIO.output(m1_pins[1], GPIO.HIGH)
-                GPIO.output(m2_pins[1], GPIO.LOW)
-                GPIO.output(m2_pins[0], GPIO.HIGH)
-            else:
-                GPIO.output(m1_pins[1], GPIO.HIGH)
-                GPIO.output(m1_pins[0], GPIO.LOW)
-                GPIO.output(m2_pins[1], GPIO.HIGH)
-                GPIO.output(m2_pins[0], GPIO.LOW)
+                centros = []
+                for c in [linea_der, linea_izq]:
+                    # compute the center of the contour
+                    M = cv2.moments(c)
+                    cX = int(M["m10"] / M["m00"])
+                    cY = int(M["m01"] / M["m00"])
+                    centros.append((cX, cY))
+                    # draw the contour and center of the shape on the image
+                    cv2.circle(frame, (cX, cY), 7, (0, 0, 255), -1)
 
-        cv2.imshow("img", frame)
-        # cv2.imshow("lineas", lineas)
+                centro_final = (
+                    (centros[0][0] + centros[1][0]) / 2,
+                    (centros[0][1] + centros[1][1]) / 2,
+                )
+                cv2.circle(
+                    frame,
+                    (int(centro_final[0]), int(centro_final[1])),
+                    7,
+                    (255, 0, 0),
+                    -1,
+                )
 
-        if cv2.waitKey(1) & 0xFF == ord("q"):
-            cv2.destroyAllWindows()
-            GPIO.cleanup()
-            break
+                print(centro_final[0], ancho / 2)
+                peso = 30
+
+                if centro_final[0] < ancho / 2 - peso:
+                    GPIO.output(m1_pins[0], GPIO.HIGH)
+                    GPIO.output(m1_pins[1], GPIO.LOW)
+                    GPIO.output(m2_pins[1], GPIO.HIGH)
+                    GPIO.output(m2_pins[0], GPIO.LOW)
+                elif centro_final[0] > ancho / 2 + peso:
+                    GPIO.output(m1_pins[0], GPIO.LOW)
+                    GPIO.output(m1_pins[1], GPIO.HIGH)
+                    GPIO.output(m2_pins[1], GPIO.LOW)
+                    GPIO.output(m2_pins[0], GPIO.HIGH)
+                else:
+                    GPIO.output(m1_pins[1], GPIO.HIGH)
+                    GPIO.output(m1_pins[0], GPIO.LOW)
+                    GPIO.output(m2_pins[1], GPIO.HIGH)
+                    GPIO.output(m2_pins[0], GPIO.LOW)
+
+            cv2.imshow("img", frame)
+            # cv2.imshow("lineas", lineas)
+
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                cv2.destroyAllWindows()
+                GPIO.cleanup()
+                break
+    except KeyboardInterrupt:
+        cv2.destroyAllWindows()
+        GPIO.cleanup()
 
 
 if __name__ == "__main__":
