82e74769046514eb7526cdfe96f83c71a60d2073
AgustinZavalaA <56318577+AgustinZavalaA@users.noreply.github.com>
2022-08-06 15:35:37 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,11 +7,21 @@ def main() -> None:
 
     m1_pins = (13, 15)
     m2_pins = (35, 37)
+    pwm_pin = 12
 
     GPIO.setup(m1_pins[0], GPIO.OUT)
     GPIO.setup(m1_pins[1], GPIO.OUT)
     GPIO.setup(m2_pins[0], GPIO.OUT)
     GPIO.setup(m2_pins[1], GPIO.OUT)
+
+    # Set GPIO pin 12 to output mode.
+    GPIO.setup(pwm_pin, GPIO.OUT)
+    # Initialize PWM on pwmPin 100Hz frequency
+    pwm = GPIO.PWM(pwm_pin, 100)
+
+    pwm.start(0)
+
+    pwm.ChangeDutyCycle(50)
 
     # for i in range(3):
     #     GPIO.output(m1_pins[0], GPIO.HIGH)
