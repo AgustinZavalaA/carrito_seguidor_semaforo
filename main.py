36564b71fcc2f7015f8788b5d6bf9f07b92faa65
AgustinZavalaA <56318577+AgustinZavalaA@users.noreply.github.com>
2022-08-06 11:50:21 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,19 +6,30 @@ def main() -> None:
     GPIO.setmode(GPIO.BOARD)
 
     m1_pins = (13, 15)
+    m2_pins = (35, 37)
 
     GPIO.setup(m1_pins[0], GPIO.OUT)
     GPIO.setup(m1_pins[1], GPIO.OUT)
+    GPIO.setup(m2_pins[0], GPIO.OUT)
+    GPIO.setup(m2_pins[1], GPIO.OUT)
 
     for i in range(3):
         GPIO.output(m1_pins[0], GPIO.HIGH)
         GPIO.output(m1_pins[1], GPIO.LOW)
+        GPIO.output(m2_pins[0], GPIO.HIGH)
+        GPIO.output(m2_pins[1], GPIO.LOW)
         time.sleep(1)
+
         GPIO.output(m1_pins[0], GPIO.LOW)
         GPIO.output(m1_pins[1], GPIO.HIGH)
+        GPIO.output(m2_pins[0], GPIO.LOW)
+        GPIO.output(m2_pins[1], GPIO.HIGH)
         time.sleep(1)
+
         GPIO.output(m1_pins[0], GPIO.LOW)
         GPIO.output(m1_pins[1], GPIO.LOW)
+        GPIO.output(m2_pins[0], GPIO.LOW)
+        GPIO.output(m2_pins[1], GPIO.LOW)
         time.sleep(1)
 
 
