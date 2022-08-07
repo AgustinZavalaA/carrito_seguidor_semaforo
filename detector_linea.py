ef40d91e805a8c7eed82aa4f2fd92a679c6892ec
AgustinZavalaA <56318577+AgustinZavalaA@users.noreply.github.com>
2022-08-06 20:43:58 -0500

    Update detector_linea.py

--- detector_linea.py
+++ detector_linea.py
@@ -103,7 +103,7 @@ def main() -> None:
                 )
 
                 print(ancho / 2 - centro_final[0])
-                peso = 50
+                peso = 30
 
                 pwm.ChangeDutyCycle(45)
                 if centro_final[0] > ancho / 2 + peso:
@@ -117,7 +117,7 @@ def main() -> None:
                     GPIO.output(m2_pins[1], GPIO.LOW)
                     GPIO.output(m2_pins[0], GPIO.HIGH)
                 else:
-                    pwm.ChangeDutyCycle(40)
+                    pwm.ChangeDutyCycle(38)
                     GPIO.output(m1_pins[1], GPIO.HIGH)
                     GPIO.output(m1_pins[0], GPIO.LOW)
                     GPIO.output(m2_pins[1], GPIO.HIGH)
